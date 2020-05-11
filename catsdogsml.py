e99ac7a18fa91d189a7946121fa40e059b36fa4c
bryantong23 <40215730+bryantong23@users.noreply.github.com>
2020-05-11 17:07:22 -0400

    Update catsdogsml.py

--- catsdogsml.py
+++ catsdogsml.py
@@ -30,8 +30,6 @@ model.add(Activation("relu"))
 model.add(MaxPooling2D(pool_size = (2,2)))
 
 model.add(Flatten())
-# model.add(Dense(64))
-# model.add(Activation("relu"))
 
 model.add(Dense(1))
 model.add(Activation("sigmoid"))
